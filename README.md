d4b2c3b9208cd6f69b276b1273c438e3a14bcbad
Florian Eckerstorfer <florian@eckerstorfer.net>
2021-04-05 16:40:11 +0200

    Prepare plugin for distribution

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 Instead of controlling media playback on your Mac, this plugin allows you to directly control Music.app.
 
+## Installation
+
+- Make sure that Node is installed and available under `/usr/local/bin/node`
+
 ## Included actions
 
 - **Play/Pause**: action with two states that toggle between play and pause. The icon shows the action that will be executed when pressed.
--- plugin/main.js
+++ plugin/main.js
@@ -1,26 +1,33 @@
-#!/Users/fec/.fnm/current/bin/node
+#!/usr/local/bin/node
 
-const ws = require("ws");
-const { writeFileSync } = require("fs");
-const { execSync } = require("child_process");
+const ws = require('ws');
+const os = require('os');
+const { writeFileSync } = require('fs');
+const { execSync } = require('child_process');
 
-const LOG_FILE_NAME =
-  "/Users/fec/Library/Logs/StreamDeck/ec.florian.musicapp.sdPlugin";
+const DEBUG = false;
+
+const LOG_FILE_NAME = `/Users/${
+  os.userInfo().username
+}/Library/Logs/StreamDeck/ec.florian.musicapp.sdPlugin`;
 
 const OSA_GET_PLAYER_STATE = 'tell application "Music" to get player state';
 const OSA_SET_PLAYER_STATE = (state) => `tell application "Music" to ${state}`;
 
 function log(...messages) {
+  if (!DEBUG) {
+    return;
+  }
   const time = new Date();
   const timeString =
-    time.toLocaleDateString() + " " + time.toLocaleTimeString();
-  const fullMessage = `${timeString}: ${messages.join(" ")}`;
+    time.toLocaleDateString() + ' ' + time.toLocaleTimeString();
+  const fullMessage = `${timeString}: ${messages.join(' ')}`;
   console.log(fullMessage);
-  writeFileSync(LOG_FILE_NAME, `${fullMessage}\n`, { flag: "a" });
+  writeFileSync(LOG_FILE_NAME, `${fullMessage}\n`, { flag: 'a' });
 }
 
 function runOsaScript(script) {
-  return execSync(`osascript -e '${script}'`, { encoding: "utf8" }).trim();
+  return execSync(`osascript -e '${script}'`, { encoding: 'utf8' }).trim();
 }
 
 var args = process.argv.slice(2);
@@ -35,30 +42,29 @@ var websocket = null;
 
 // Open the web socket to Stream Deck
 // Use 127.0.0.1 because Windows needs 300ms to resolve localhost
-websocket = new ws("ws://127.0.0.1:" + inPort);
+websocket = new ws('ws://127.0.0.1:' + inPort);
 
 function setState(context, state) {
   if (websocket) {
     var json = {
-      event: "setState",
+      event: 'setState',
       context: context,
       payload: {
         state: state,
       },
     };
-    log("setState", JSON.stringify(json));
     websocket.send(JSON.stringify(json));
   }
 }
 
 function updatePlayerState(context) {
   const osaPlayerState = runOsaScript(OSA_GET_PLAYER_STATE);
-  const currentPlayerState = osaPlayerState === "playing" ? 1 : 0;
+  const currentPlayerState = osaPlayerState === 'playing' ? 1 : 0;
   setState(context, currentPlayerState);
 }
 
-websocket.on("open", function () {
-  log("onOpen");
+websocket.on('open', function () {
+  log('onOpen');
   var json = {
     event: registerEvent,
     uuid: uuid,
@@ -66,13 +72,13 @@ websocket.on("open", function () {
   websocket.send(JSON.stringify(json));
 });
 
-websocket.on("message", function (eventString) {
+websocket.on('message', function (eventString) {
   const event = JSON.parse(eventString);
   let playerUpdateInterval;
-  log("onMessage", JSON.stringify(event));
+  log('onMessage', JSON.stringify(event));
   if (
-    event.event === "willAppear" &&
-    event.action === "ec.florian.musicapp.playpause"
+    event.event === 'willAppear' &&
+    event.action === 'ec.florian.musicapp.playpause'
   ) {
     try {
       updatePlayerState(event.context);
@@ -81,40 +87,40 @@ websocket.on("message", function (eventString) {
         1000
       );
     } catch (error) {
-      log("initPlayerState", "Error:", JSON.stringify(error));
+      log('initPlayerState', 'Error:', JSON.stringify(error));
     }
   } else if (
-    event.event === "willDisappear" &&
-    event.action === "ec.florian.musicapp.playpause" &&
+    event.event === 'willDisappear' &&
+    event.action === 'ec.florian.musicapp.playpause' &&
     playerUpdateInterval
   ) {
     clearInterval(playerUpdateInterval);
   } else if (
-    event.event === "keyUp" &&
-    event.action === "ec.florian.musicapp.playpause"
+    event.event === 'keyUp' &&
+    event.action === 'ec.florian.musicapp.playpause'
   ) {
     try {
       const osaPlayerState = runOsaScript(OSA_GET_PLAYER_STATE);
-      const currentPlayerState = osaPlayerState === "playing" ? 1 : 0;
+      const currentPlayerState = osaPlayerState === 'playing' ? 1 : 0;
       const targetPlayerState = currentPlayerState === 1 ? 0 : 1;
       log(
-        "osaPlayerState",
+        'osaPlayerState',
         osaPlayerState,
-        "currentPlayerState",
+        'currentPlayerState',
         currentPlayerState,
-        "targetPlayerState",
+        'targetPlayerState',
         targetPlayerState
       );
       try {
         runOsaScript(
-          OSA_SET_PLAYER_STATE(targetPlayerState ? "play" : "pause")
+          OSA_SET_PLAYER_STATE(targetPlayerState ? 'play' : 'pause')
         );
         setState(event.context, targetPlayerState);
       } catch (error) {
-        log("setPlayerState", "Error:", JSON.stringify(error));
+        log('setPlayerState', 'Error:', JSON.stringify(error));
       }
     } catch (error) {
-      log("getPlayerState", "Error:", JSON.stringify(error));
+      log('getPlayerState', 'Error:', JSON.stringify(error));
     }
   }
 });
